0fa6e9f06c1437f5effb707d19695ec8fb7b9c52
adrian.vegas <adrian.vegas@bbva.com>
2020-01-26 21:48:18 +0100

    TODO 4 done and tested - Classifying Labels as Dogs

--- practice_image_classification_for_a_city_dog_show/adjust_results4_isadog.py
+++ practice_image_classification_for_a_city_dog_show/adjust_results4_isadog.py
@@ -66,5 +66,48 @@ def adjust_results4_isadog(results_dic, dogfile):
                maltese) (string - indicates text file's filename)
     Returns:
            None - results_dic is mutable data type so no return needed.
-    """           
+    """     
+    dognames_dic = dict()
+
+    with open(dogfile, "r") as infile:
+      line = infile.readline()
+      while line != "":
+
+        # Process line by striping newline from line
+        dogname = line.rstrip()
+
+        # adds dogname(line) to dogsnames_dic if it doesn't already exist 
+        # in the dogsnames_dic dictionary
+        if(dogname not in dognames_dic):
+            dognames_dic[dogname] = 1
+
+        line = infile.readline()
+    
+    for key in results_dic:
+
+      # Pet Image Label IS of Dog (e.g. found in dognames_dic)
+      if results_dic[key][0] in dognames_dic:
+      
+        # Classifier Label IS image of Dog (e.g. found in dognames_dic)
+        # appends (1, 1) because both labels are dogs
+        if results_dic[key][1] in dognames_dic:
+          results_dic[key].extend((1, 1))
+
+        # Classifier Label IS NOT image of dog (e.g. NOT in dognames_dic)
+        # appends (1,0) because only pet label is a dog          
+        else:
+          results_dic[key].extend((1, 1))
+
+      # Pet Image Label IS NOT a Dog image (e.g. NOT found in dognames_dic)  
+      else:
+        
+        # Classifier Label IS image of Dog (e.g. found in dognames_dic)
+        # appends (0, 1)because only Classifier labe is a dog
+        if results_dic[key][1] in dognames_dic:
+          results_dic[key].extend((0, 1))
+        # Classifier Label IS NOT image of Dog (e.g. NOT in dognames_dic)
+        # appends (0, 0) because both labels aren't dogs  
+        else:
+          results_dic[key].extend((0, 0))
+
     None
