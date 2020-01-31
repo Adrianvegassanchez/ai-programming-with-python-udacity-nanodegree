8c50c7d6b0312861faffbd79d77b7bfc6187c66c
adrian.vegas <adrian.vegas@bbva.com>
2020-01-31 12:57:06 +0100

    Refactor on classify_images, replace multiple appends by extend

--- practice_image_classification_for_a_city_dog_show/classify_images.py
+++ practice_image_classification_for_a_city_dog_show/classify_images.py
@@ -74,9 +74,11 @@ def classify_images(images_dir, results_dic, model):
       truth = results_dic[key][0]
 
       if truth in model_label:
-        results_dic[key].append(model_label)
-        results_dic[key].append(1)
+        #results_dic[key].append(model_label)
+        #results_dic[key].append(1)
+        results_dic[key].extend([model_label,1])
       else:
-        results_dic[key].append(model_label)
-        results_dic[key].append(0)
+        #results_dic[key].append(model_label)
+        #results_dic[key].append(0)
+        results_dic[key].extend([model_label,0])
 
